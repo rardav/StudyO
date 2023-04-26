781c1c0287b19102cba5ad0e16c4140d6fc09067
Rares David <david_rares@yahoo.com>
2023-04-27 01:10:00 +0300

    new web-spa proj

--- src/Web/web-spa/src/app/app.module.ts
+++ src/Web/web-spa/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenubarModule } from 'primeng/menubar'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MenubarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
