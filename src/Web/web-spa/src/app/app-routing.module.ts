686f62a5cadfb5162bae09c00e554a62c1b77b7a
Rares David <david_rares@yahoo.com>
2023-04-28 01:32:26 +0300

    ds

--- src/Web/web-spa/src/app/app-routing.module.ts
+++ src/Web/web-spa/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { QuizzesComponent } from './pages/quizzes/quizzes.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'courses', component: CoursesComponent},
+  { path: 'quizzes', component: QuizzesComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Web/web-spa/src/app/app.module.ts
+++ src/Web/web-spa/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenubarModule } from 'primeng/menubar'
+import { ButtonModule } from 'primeng/button'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { QuizzesComponent } from './pages/quizzes/quizzes.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CoursesComponent,
+    QuizzesComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
