a13e5d3e1c2b9ee85d25e9ebebf8d820a00a9fca
Rares David <david_rares@yahoo.com>
2023-04-30 01:59:41 +0300

    client updates

--- src/Web/web-spa/src/app/app.module.ts
+++ src/Web/web-spa/src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MenubarModule } from 'primeng/menubar'
-import { ButtonModule } from 'primeng/button'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { QuizzesComponent } from './pages/quizzes/quizzes.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { GroupsComponent } from './pages/groups/groups.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +23,16 @@ import { SignupComponent } from './pages/signup/signup.component';
     CoursesComponent,
     QuizzesComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    GroupsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FontAwesomeModule,
     AppRoutingModule,
-    MenubarModule,
-    ButtonModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
